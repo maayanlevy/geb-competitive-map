b292a0f17910112388e924a95ff7eee18c8df452
Maayan Levy <maayanlvy@gmail.com>
2024-10-07 10:29:22 +0300

    set up credentials from st env

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,18 +10,7 @@ st.set_page_config(layout="wide", page_title="Competitive Map")
 
 # Set up Google Sheets API credentials
 creds = service_account.Credentials.from_service_account_info(
-    {
-        "type": st.secrets["type"],
-        "project_id": st.secrets["project_id"],
-        "private_key_id": st.secrets["private_key_id"],
-        "private_key": st.secrets["private_key"],
-        "client_email": st.secrets["client_email"],
-        "client_id": st.secrets["client_id"],
-        "auth_uri": st.secrets["auth_uri"],
-        "token_uri": st.secrets["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
-    },
+    st.secrets["gcp_service_account"],
     scopes=['https://www.googleapis.com/auth/spreadsheets.readonly']
 )
 
