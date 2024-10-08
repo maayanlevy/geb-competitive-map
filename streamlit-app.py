99f8c1c890385b504e8f7cfc88100ce5e15cd216
Maayan Levy <maayanlvy@gmail.com>
2024-10-08 15:52:05 +0300

    58

--- streamlit-app.py
+++ streamlit-app.py
@@ -180,7 +180,7 @@ def create_competitive_map(df):
         "Customized AI": (30, 30),
         "Enterprise Search": (25, 80),
         "Task-Sepcific Agents": (5, 80),
-        "Vertical AI": (30, 60),
+        "Vertical AI": (30, 58),
         "UI Models": (60, 58),
         "Reasoning Models": (60, 75),
         "DIY AI": (65, 30)
